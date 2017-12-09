db48cd6b6e9fdc912435c05f0aa7e54060f592e3
qiaoyu <qiaoyu1111358@gmail.com>
2017-12-10 04:06:23 +0800

    implement backend CRUD

---
+++ app/controllers/admin/courses_controller.rb
@@ -0,0 +1,39 @@
+class Admin::CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+
+    if @course.update(course_params)
+      redirect_to admin_courses_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to admin_courses_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description)
+  end
+end
