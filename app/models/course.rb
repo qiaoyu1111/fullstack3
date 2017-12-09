be586ee97ad7278428365028d6b01ab85925514a
qiaoyu <qiaoyu1111358@gmail.com>
2017-12-10 07:13:30 +0800

    add post and hook course/user relation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :title, presence: true
+
+  belongs_to :user
+  has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209212838) do
+ActiveRecord::Schema.define(version: 20171209231140) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20171209212838) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.boolean "is_hidden", default: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.integer "course_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
