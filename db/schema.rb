95e7bbc21a06c42fdc1eb1c6e91de0f0e17673c6
qiaoyu <qiaoyu1111358@gmail.com>
2017-12-12 06:20:17 +0800

    add post#name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209231140) do
+ActiveRecord::Schema.define(version: 20171211220054) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171209231140) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
