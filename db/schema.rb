efc06fa2c4717305e2df8c44552e3dca7b80514f
qiaoyu <qiaoyu1111358@gmail.com>
2017-12-13 16:51:12 +0800

    add posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213020318) do
+ActiveRecord::Schema.define(version: 20171213084916) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20171213020318) do
     t.string "image"
     t.boolean "is_hidden", default: true
     t.integer "user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.integer "course_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
